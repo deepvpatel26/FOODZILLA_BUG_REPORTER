9eb2e1aafad9f7f41b22d0f4566da652f6542d7d
Deep Patel <deepvpatel47@outlook.com>
2021-04-16 19:05:28 +0530

    version 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace bugs_report_page_final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Desctxt = new System.Windows.Forms.TextBox();
+            this.Subtxt = new System.Windows.Forms.TextBox();
+            this.Emailtxt = new System.Windows.Forms.TextBox();
+            this.Userid = new System.Windows.Forms.TextBox();
+            this.Nametxt = new System.Windows.Forms.TextBox();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(112)))));
+            this.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button1.Font = new System.Drawing.Font("Segoe UI Symbol", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button1.ForeColor = System.Drawing.Color.White;
+            this.Button1.Location = new System.Drawing.Point(358, 461);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(124, 40);
+            this.Button1.TabIndex = 18;
+            this.Button1.Text = "SUBMIT";
+            this.Button1.UseVisualStyleBackColor = false;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Desctxt
+            // 
+            this.Desctxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(78)))), ((int)(((byte)(97)))));
+            this.Desctxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Desctxt.Font = new System.Drawing.Font("Gadugi", 10F);
+            this.Desctxt.ForeColor = System.Drawing.SystemColors.Window;
+            this.Desctxt.Location = new System.Drawing.Point(182, 308);
+            this.Desctxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Desctxt.Multiline = true;
+            this.Desctxt.Name = "Desctxt";
+            this.Desctxt.Size = new System.Drawing.Size(440, 133);
+            this.Desctxt.TabIndex = 13;
+            // 
+            // Subtxt
+            // 
+            this.Subtxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(78)))), ((int)(((byte)(97)))));
+            this.Subtxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Subtxt.Font = new System.Drawing.Font("Gadugi", 10F);
+            this.Subtxt.ForeColor = System.Drawing.SystemColors.Window;
+            this.Subtxt.Location = new System.Drawing.Point(182, 234);
+            this.Subtxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Subtxt.Multiline = true;
+            this.Subtxt.Name = "Subtxt";
+            this.Subtxt.Size = new System.Drawing.Size(440, 47);
+            this.Subtxt.TabIndex = 14;
+            // 
+            // Emailtxt
+            // 
+            this.Emailtxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(78)))), ((int)(((byte)(97)))));
+            this.Emailtxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Emailtxt.Font = new System.Drawing.Font("Gadugi", 10F);
+            this.Emailtxt.ForeColor = System.Drawing.SystemColors.Window;
+            this.Emailtxt.Location = new System.Drawing.Point(182, 167);
+            this.Emailtxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Emailtxt.Name = "Emailtxt";
+            this.Emailtxt.Size = new System.Drawing.Size(440, 23);
+            this.Emailtxt.TabIndex = 15;
+            // 
+            // Userid
+            // 
+            this.Userid.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(78)))), ((int)(((byte)(97)))));
+            this.Userid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Userid.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Userid.Font = new System.Drawing.Font("Gadugi", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Userid.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.Userid.Location = new System.Drawing.Point(182, 67);
+            this.Userid.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Userid.Name = "Userid";
+            this.Userid.Size = new System.Drawing.Size(440, 23);
+            this.Userid.TabIndex = 16;
+            // 
+            // Nametxt
+            // 
+            this.Nametxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(78)))), ((int)(((byte)(97)))));
+            this.Nametxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Nametxt.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Nametxt.Font = new System.Drawing.Font("Gadugi", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nametxt.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.Nametxt.Location = new System.Drawing.Point(182, 117);
+            this.Nametxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Nametxt.Name = "Nametxt";
+            this.Nametxt.Size = new System.Drawing.Size(440, 23);
+            this.Nametxt.TabIndex = 17;
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Font = new System.Drawing.Font("Segoe UI Symbol", 10.2F, System.Drawing.FontStyle.Bold);
+            this.Label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(255)))), ((int)(((byte)(251)))));
+            this.Label5.Location = new System.Drawing.Point(178, 283);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(176, 23);
+            this.Label5.TabIndex = 8;
+            this.Label5.Text = "Describe your Issue";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Font = new System.Drawing.Font("Segoe UI Symbol", 10.2F, System.Drawing.FontStyle.Bold);
+            this.Label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(255)))), ((int)(((byte)(251)))));
+            this.Label4.Location = new System.Drawing.Point(178, 209);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(73, 23);
+            this.Label4.TabIndex = 9;
+            this.Label4.Text = "Subject";
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Font = new System.Drawing.Font("Segoe UI Symbol", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(255)))), ((int)(((byte)(251)))));
+            this.Label6.Location = new System.Drawing.Point(178, 42);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(76, 23);
+            this.Label6.TabIndex = 10;
+            this.Label6.Text = "User id ";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("Segoe UI Symbol", 10.2F, System.Drawing.FontStyle.Bold);
+            this.Label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(255)))), ((int)(((byte)(251)))));
+            this.Label3.Location = new System.Drawing.Point(178, 142);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(56, 23);
+            this.Label3.TabIndex = 11;
+            this.Label3.Text = "Email";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("Segoe UI Symbol", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(255)))), ((int)(((byte)(251)))));
+            this.Label2.Location = new System.Drawing.Point(178, 92);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(60, 23);
+            this.Label2.TabIndex = 12;
+            this.Label2.Text = "Name";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Mongolian Baiti", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(255)))), ((int)(((byte)(251)))));
+            this.Label1.Location = new System.Drawing.Point(265, -24);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(310, 30);
+            this.Label1.TabIndex = 7;
+            this.Label1.Text = "Share Your Problems !!!";
+            this.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label7
+            // 
+            this.Label7.AutoSize = true;
+            this.Label7.Font = new System.Drawing.Font("Mongolian Baiti", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(255)))), ((int)(((byte)(251)))));
+            this.Label7.Location = new System.Drawing.Point(281, 8);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(310, 30);
+            this.Label7.TabIndex = 19;
+            this.Label7.Text = "Share Your Problems !!!";
+            this.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(62)))), ((int)(((byte)(78)))));
+            this.ClientSize = new System.Drawing.Size(830, 536);
+            this.Controls.Add(this.Label7);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.Desctxt);
+            this.Controls.Add(this.Subtxt);
+            this.Controls.Add(this.Emailtxt);
+            this.Controls.Add(this.Userid);
+            this.Controls.Add(this.Nametxt);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Foodzilla";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.TextBox Desctxt;
+        private System.Windows.Forms.TextBox Subtxt;
+        private System.Windows.Forms.TextBox Emailtxt;
+        private System.Windows.Forms.TextBox Userid;
+        private System.Windows.Forms.TextBox Nametxt;
+        private System.Windows.Forms.Label Label5;
+        private System.Windows.Forms.Label Label4;
+        private System.Windows.Forms.Label Label6;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label Label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Tulpep.NotificationWindow;
+using System.Data.Sql;
+
+
+
+namespace bugs_report_page_final
+{
+    public partial class Form1 : Form
+    {
+        SqlCommand cmd;
+        SqlConnection con;
+        public Form1()
+        {
+            InitializeComponent();
+            this.ActiveControl = Userid;
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (Nametxt.Text == "")
+                MessageBox.Show("Please Enter your name");
+            else if (Emailtxt.Text == "")
+                MessageBox.Show("Please Enter your Email");
+            else if (Userid.Text == "")
+                MessageBox.Show("Please Enter your User id");
+            else if (Subtxt.Text == "")
+                MessageBox.Show("Please Enter your problem");
+            {
+                con = new SqlConnection(@"Data Source=LAPTOP-UNKNOWN\TEW_SQLEXPRESS;Initial Catalog=bugs_data;Integrated Security=True");
+                con.Open();
+                cmd = new SqlCommand("Insert into bugs_tbl(Username,mail,Sub,Details,user_id) Values(@name,@mail,@sub,@details,@u)", con);
+                cmd.Parameters.AddWithValue(@"name", Nametxt.Text);
+                cmd.Parameters.AddWithValue(@"mail", Emailtxt.Text);
+                cmd.Parameters.AddWithValue(@"sub", Subtxt.Text);
+                cmd.Parameters.AddWithValue(@"details", Desctxt.Text);
+                cmd.Parameters.AddWithValue(@"u", Userid.Text);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Submitted Successfully!!!");
+                    {
+                    PopupNotifier popup = new PopupNotifier();
+                    popup.Image = Properties.Resources.info;
+                    popup.TitleText = "\\Foodzilla\\";
+                    popup.ContentText = "submitted succeffully! Sit back and relax!!!";
+                    popup.Popup();
+                    }
+                Clear();
+
+            }
+            void Clear ()
+            {
+                Nametxt.Text = Emailtxt.Text = Subtxt.Text = Desctxt.Text = Userid.Text = "";
+            }
+
+
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
